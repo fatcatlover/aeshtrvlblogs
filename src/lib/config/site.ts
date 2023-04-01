7b37e11ed63515c3961fd910ac74111c2f331bde
fatcatlover <123166987+fatcatlover@users.noreply.github.com>
2023-04-01 18:32:18 +0800

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,15 +3,15 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  title: 'Travel Blohs',
+  subtitle: 'Welcome to my blog website!',
   lang: 'en-US',
-  description: 'Powered by SvelteKit/Urara',
+  description: 'Travel Blogs',
   author: {
     avatar: '/assets/maskable@512.png',
-    name: 'John Doe',
-    status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    name: 'Aesh',
+    status: '👋🏻',
+    bio: '👇🏻'
   },
   themeColor: '#3D4451'
 }
