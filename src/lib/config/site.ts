c6f3eef6f0c726d57d8394b5a04456a3f1fb1dda
fatcatlover <123166987+fatcatlover@users.noreply.github.com>
2023-05-12 12:42:09 +0800

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,14 +3,14 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Travel Blohs',
+  title: 'aeshtravelblogs',
   subtitle: 'Welcome to my blog website!',
   lang: 'en-US',
-  description: 'Travel Blogs',
+  description: 'We have variety of blogs written! Choose one below',
   author: {
     avatar: '/assets/maskable@512.png',
     name: 'Aesh',
-    status: '👋🏻',
+    status: '',
     bio: '👇🏻'
   },
   themeColor: '#3D4451'
